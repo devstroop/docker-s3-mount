e3f46ec4b37008d49445529a7b734ac0de151184
Akash Shah <44998751+itsalfredakku@users.noreply.github.com>
2025-01-28 01:13:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Fail on any error
+set -e
+
+# Ensure the bucket name is set
+if [ -z "$AWS_BUCKET" ]; then
+  echo "Error: AWS_BUCKET environment variable is not set."
+  exit 1
+fi
+
 # Ensure the mountpoint directory exists
 mkdir -p /mountpoint/$AWS_BUCKET
 
 # Execute the mount-s3 command
-exec mount-s3 $AWS_BUCKET /mountpoint/$AWS_BUCKET --allow-other
+exec mount-s3 -f "$AWS_BUCKET" "/mountpoint/$AWS_BUCKET" --allow-other
